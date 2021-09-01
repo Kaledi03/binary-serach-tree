8a6935b46d7ad754ab9c98d38f9afd2894649426
Kaledi03 <85677264+Kaledi03@users.noreply.github.com>
2021-09-01 21:34:50 +0200

    Update binary-search-tree.py

--- binary-search-tree.py
+++ binary-search-tree.py
@@ -44,6 +44,82 @@ def addNode(head, value):
     else:
         return
 
+# Recursive function that deletes a node from the tree. Return boolean (true if removed ; false if is not removed)
+def removeNode(head, value):
+    # Base case: end of the tree and node not found
+    if head == None:
+        return False
+    
+    # If the value if equal to the current node it will be removed
+    if value == head.value:
+        # The node to remove have a left reference different from 'None' so it reference a real node
+        if head.left != None:
+            tempNodeLeft = head.left
+            tempNodeLeft2 = head
+
+            # Iterates untill it finds the greates value on the right of the Node to remove
+            while tempNodeLeft.right != None:
+                tempNodeLeft2 = tempNodeLeft
+                tempNodeLeft = tempNodeLeft.right
+            # Check if the value that will take the place of the one to remove have a valid reference on the left
+            if tempNodeLeft.left != None:
+                head.value = tempNodeLeft.value
+                tempNodeLeft.value = tempNodeLeft.left.value
+                tempNodeLeft.left = tempNodeLeft.left.left
+                tempNodeLeft.right = tempNodeLeft.left.right
+            else:
+                head.value = tempNodeLeft.value
+                tempNode2Left.right = None
+        # The node to remove have a valid node on the right
+        elif head.right != None:
+            tempNodeRight = head.right
+            tempNodeRight2 = head
+            # Iterates untill it finds the lowest value on the left of the Node to remove
+            while tempNodeRight.left != None:
+                tempNodeRight2 = tempNodeRight
+                tempNodeRight = tempNodeRight.left
+            # Check if the value that will take the place of the one to remove have a valid reverence on the right
+            if tempNodeRight.right != None:
+                head.value = tempNodeRight.value
+                tempNodeRight.value = tempNodeRight.right.value
+                tempNodeRight.left = tempNodeRight.right.left
+                tempNodeRight.right = tempNodeRight.right.right
+            else:
+                head.value = tempNodeRight.value
+                tempNodeRight2.left = None
+        # The node to remove is the last node of the tree
+        else:
+            head.value = None
+
+        return True
+
+    # If the value is greater than the value of the current node the program will search for it in the right part of the tree 
+    # by recursively calling the 'removeNode' function
+    elif value > head.value:
+        return removeNode(head.right, value)
+    
+    # The value is less than the value of the head so the program will search in the left part of the tree
+    else:
+        return removeNode(head.left, value)
+
+# Recursive function that check if a value is present in the tree
+def findNode(head, value):
+    # Base case: if the end of the tree is reached without finding the value the program will return false (not find)
+    if head == None:
+        return False
+
+    # If the value is equal to the value of the present node the program will return true (find)
+    elif head.value == value:
+        return True
+    
+    # The value is less than the node so it should be on the left
+    elif head.value > value:
+        return findNode(head.left, value)
+
+    # The value is greater than the node so it should be on the right
+    else:
+        return findNode(head.right, value)
+
 # Recursive function that prints the values of the tree in order
 def printTree(head):
     # Base case (end of the tree)
